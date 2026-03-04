1cc3e0540e754d841ef265c02663823477f9e01a
maksim12 <maksi.volodin.05@mail.ru>
2026-03-04 14:10:52 +0300

    fixed run

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import uvicorn
 
+from appv1.config import settings
+
 if __name__ == "__main__":
     uvicorn.run(
         "appv1.main:app",
-        host="127.0.0.1",
-        port=8000,
+        host=settings.HOST,
+        port=settings.PORT,
     )
